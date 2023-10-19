b0a5f09f8b5d07436b78c1fc44763c37153a2dc3
Marc Del Castillo <dev@del-castillo.fr>
2023-10-19 12:52:09 +0200

    02 - Les actions

--- functions.php
+++ functions.php
@@ -1,2 +1,20 @@
 <?php
-add_theme_support('title-tag');
+
+function montheme_supports()
+{
+    add_theme_support('title-tag');
+}
+
+function montheme_register_assets()
+{
+    wp_register_style('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css');
+    wp_register_script('bootstrap', 'https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.min.js', ['popper', 'jquery'], false, true);
+    wp_register_script('popper', 'https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js', [], false, true);
+    wp_deregister_script('jquery');
+    wp_register_script('jquery', 'https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js', [], false, true);
+    wp_enqueue_style('bootstrap');
+    wp_enqueue_script('bootstrap');
+}
+
+add_action('after_setup_theme', 'montheme_supports');
+add_action('wp_enqueue_scripts', 'montheme_register_assets');

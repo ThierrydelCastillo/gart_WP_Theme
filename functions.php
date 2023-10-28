2d04ffdf8a1bf05e6c50693df17d89a8c14d3650
Marc Del Castillo <dev@del-castillo.fr>
2023-10-28 18:25:05 +0200

    12 - Les taxonomies

--- functions.php
+++ functions.php
@@ -68,10 +68,31 @@ function montheme_pagination()
     echo '</nav>';
 }
 
+function montheme_init()
+{
+    register_taxonomy('sport', 'post', [
+        'labels' => [
+            'name' => 'Sport',
+            'singular_name' => 'Sport',
+            'plural_name' => 'Sports',
+            'search_items' => 'Rechercher des sports',
+            'all_items' => 'Tous les sports',
+            'edit_item' => 'Editer le sport',
+            'update_item' => 'Modifier le sport',
+            'add_new_item' => 'Ajouter un nouveau sport',
+            'new_item_name' => 'Ajouter un nouveau sport',
+            'Menu_name' => 'Sport',
+        ],
+        'show_in_rest' => true,
+        'hierarchical' => true,
+        'show_admin_column' => true,
+    ]);
+}
+
+add_action('init', 'montheme_init');
 add_action('after_setup_theme', 'montheme_supports');
 add_action('wp_enqueue_scripts', 'montheme_register_assets');
-add_action('document_title_separator', 'montheme_title_separator');
-add_action('document_title_parts', 'montheme_document_title_parts');
+add_filter('document_title_separator', 'montheme_title_separator');
 add_filter('nav_menu_css_class', 'montheme_menu_class');
 add_filter('nav_menu_link_attributes', 'montheme_menu_link_class');
 
--- taxonomy-sport.php
+++ taxonomy-sport.php
@@ -1,19 +1,26 @@
 <?php get_header() ?>
 
+<h1><?= esc_html(get_queried_object()->name) ?></h1>
+
+<p>
+    <?= esc_html(get_queried_object()->description) ?>
+</p>
+
 <?php //wp_list_categories(['taxonomy' => 'sport', 'title_li' => '']) 
 ?>
 
 <?php $sports = get_terms(['taxonomy' => 'sport']) ?>
-
-<ul class="nav nav-pills my-4">
-    <?php foreach ($sports as $sport) : ?>
-        <li class="nav-item">
-            <a href="<?= get_term_link($sport) ?>" class="nav-link <?= is_tax('sport', $sport->term_id) ? 'active' : '' ?>">
-                <?= $sport->name ?>
-            </a>
-        </li>
-    <?php endforeach ?>
-</ul>
+<?php if (is_array($sports)) : ?>
+    <ul class="nav nav-pills my-4">
+        <?php foreach ($sports as $sport) : ?>
+            <li class="nav-item">
+                <a href="<?= get_term_link($sport) ?>" class="nav-link <?= is_tax('sport', $sport->term_id) ? 'active' : '' ?>">
+                    <?= $sport->name ?>
+                </a>
+            </li>
+        <?php endforeach ?>
+    </ul>
+<?php endif ?>
 
 <?php if (have_posts()) : ?>
     <div class="row">

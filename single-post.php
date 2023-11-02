44dda8fe4b4fbefc79f20d944519c0b04a92ea1a
Marc Del Castillo <dev@del-castillo.fr>
2023-11-02 13:12:59 +0100

    17 - L'objet WP_Query

--- single-post.php
+++ single-post.php
@@ -12,6 +12,40 @@
 
         <?php the_post_thumbnail() ?>
         <?php the_content() ?>
+
+        <h2>Articles relatifs</h2>
+        <div class="row">
+            <?php
+            $sports = array_map(function ($term) {
+                return $term->term_id;
+            }, get_the_terms(get_post(), 'sport'));
+            $query = new WP_Query([
+                'post__not_in' => [get_the_ID()],
+                'post_type' => 'post',
+                'posts_per_page' => 3,
+                'orderby' => 'rand',
+                'tax_query' => [
+                    [
+                        'taxonomy' => 'sport',
+                        'terms' => $sports,
+                    ],
+                ],
+                'meta_query' => [
+                    [
+                        'key' => SponsoMetaBox::META_KEY,
+                        'compare' => 'EXISTS',
+                    ]
+                ],
+            ]);
+            //var_dump($query->get_posts());
+            while ($query->have_posts()) : $query->the_post();
+            ?>
+                <div class="col-sm-4">
+                    <?php get_template_part('parts/card', 'post') ?>
+                </div>
+            <?php endwhile;
+            wp_reset_postdata(); ?>
+        </div>
     <?php endwhile ?>
 <?php endif ?>
 
